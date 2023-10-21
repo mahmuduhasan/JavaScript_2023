0f2caf12d305f9a8ce9723f4a4bfba2486f92db7
Mahmudul Hasan <mahmuduhasan>
2023-10-22 00:51:42 +0600

    Basic Done

--- script.js
+++ script.js
@@ -887,3 +887,66 @@ const addPrice = products
   .reduce((acc, curr) => acc + curr);
 
 console.log(addPrice);
+
+const people = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@gmail.com",
+    phone: "111-111-1111",
+    age: 30,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Poe",
+    email: "jane@gmail.com",
+    phone: "222-222-2222",
+    age: 25,
+  },
+  {
+    firstName: "Bob",
+    lastName: "Foe",
+    email: "bob@gmail.com",
+    phone: "333-333-3333",
+    age: 45,
+  },
+  {
+    firstName: "Sara",
+    lastName: "Soe",
+    email: "Sara@gmail.com",
+    phone: "444-444-4444",
+    age: 19,
+  },
+  {
+    firstName: "Jose",
+    lastName: "Koe",
+    email: "jose@gmail.com",
+    phone: "555-555-5555",
+    age: 23,
+  },
+];
+
+const youngPeople = people
+  .filter((person) => person.age <= 25)
+  .map((youngPerson) => ({
+    name: youngPerson.firstName + " " + youngPerson.lastName,
+    email: youngPerson.email,
+  }));
+
+console.log(youngPeople);
+
+const challengeNumbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = challengeNumbers
+  .filter((num) => num > 0)
+  .reduce((acc, curr) => acc + curr);
+
+console.log(positiveSum);
+
+const words = ["coder", "programmer", "developer"];
+
+const capitalizedWords = words.map((word) => {
+  return word[0].toUpperCase() + word.slice(1);
+});
+
+console.log(capitalizedWords);
